951bf07c7dfd157a451b3d0cc89fabfa8b8d9af1
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-10 19:02:08 +0000

    add elements of sidebar and footer 2 spacing

--- wp-content/themes/My First Template WP/index.php
+++ wp-content/themes/My First Template WP/index.php
@@ -116,22 +116,39 @@
 		 </div><!--fim do games-->
 	 
 	 </div><!--fim do content-->
+
 <div id="sidebar">
-     <div id="sidebar-vistos">
-	     sidebar vistos
+     
+	 <div id="sidebar-vistos">
+	     
+		 sidebar vistos
+	 
 	 </div><!--fim da sidebar vistos-->
+	 
 	 <div id="sidebar-colunistas">
-	     sidebar colunistas
+	     
+		 sidebar colunistas
+	 
 	 </div><!--fim da sidebar colunistas-->
+	 
 	 <div id="sidebar-publi">
+	   
 	     sidebar publicidades
+	
 	 </div><!--fim da sidebar publi-->
+	 
 	 <div id="sidebar-coment">
+	   
 	     mais comentados
+	 
 	 </div><!--fim da sidebar coment-->
+	 
 	 <div id="sidebar-facebook">
-	     sidebar facebook
+	     
+		 sidebar facebook
+	 
 	 </div><!--fim da sidebar facebook-->
+
 </div><!--fim sidebar-->	 
 
 </div><!--fim do container-->
