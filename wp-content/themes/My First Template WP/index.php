5b0739da6425bba50f779aee5a9cdab6decb2ad1
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-11 11:59:30 +0000

    Configuration  destaque and content in header.php

--- wp-content/themes/My First Template WP/index.php
+++ wp-content/themes/My First Template WP/index.php
@@ -6,7 +6,59 @@
 	     
 		 <div id="destaque">
 		    
-			Destaque
+			<div class="destaque-post">
+			     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post.jpg" rel="" title="" /></a>
+				 <div class="destaque-info">
+				     <ul>
+					     <li class="dest-autor">autor</li>
+						 <li class="dest-views">views</li>
+						 <li class="dest-coment">coment</li>
+					 </ul>
+			     </div><!--fim destaque info-->
+				 
+				 <h1><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h1>
+				 
+				 <div class="list-dest">
+				     <ul>
+					     <li>
+						     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post2.jpg" rel="" title=""/></a>
+							 <h2><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h2>
+						 </li>
+						  <li>
+						     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post2.jpg" rel="" title=""/></a>
+							 <h2><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h2>
+						 </li>
+					 </ul>
+			     </div><!--fim list dest-->
+				 
+			</div><!--fim destaque post-->
+			<div class="destaque-post right ">
+			     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post.jpg" rel="" title="" /></a>
+				 <div class="destaque-info">
+				     <ul>
+					     <li class="dest-autor">autor</li>
+						 <li class="dest-views">views</li>
+						 <li class="dest-coment">coment</li>
+					 </ul>
+			     </div><!--fim destaque info-->
+				 
+				 <h1><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h1>
+				 
+				 <div class="list-dest">
+				     <ul>
+					     <li>
+						     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post2.jpg" rel="" title=""/></a>
+							 <h2><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h2>
+						 </li>
+						  <li>
+						     <a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/post2.jpg" rel="" title=""/></a>
+							 <h2><a href="#">Windows 8.1 pode ter volta do botão inicial</a></h2>
+						 </li>
+					 </ul>
+			     </div><!--fim list dest-->
+				 
+			</div><!--fim destaque post-->
+			
 		 
 		 </div><!--fim do destaque-->
 		 
