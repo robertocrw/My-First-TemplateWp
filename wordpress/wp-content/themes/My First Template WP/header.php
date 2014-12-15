aa8bd21d34ea38afb185a6db3c4f336a3fce64ac
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-15 16:47:27 +0000

    Just commit

---
+++ wordpress/wp-content/themes/My First Template WP/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--language_atributes vai fazer com que ele pegue as informação de linguage do usuario -->
+<html <?php language_attributes()?> >
+<!--bloginfo('charset') vai fazer com que atualize para a timezone do usuario ex: utf-8-->
+<!--wp_title('-',true,'right'); bloginfo() vai fazer com que ele pegue as informações do titulo-->
+<!--href="<?php// bloginfo('template_url') ?>/style.css" vai fazer com que permita pegar informações da pagina de estilo css-->
+<head>
+     <meta charset="<?php bloginfo('charset') ?>" /> 
+     <title><?php wp_title('-',true,'right'); bloginfo() ?></title>
+	 
+	 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.css" />
+
+<?php wp_head();?>	 
+</head>
+<body>
+
+<!--Box Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=535815079870722&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!--Fim Box Fabeook-->
+
+<div id="header">
+     
+	 <div id="header-superior">
+	     
+		 <div id="header-superior-content">
+		     
+			 <div id="header-paginas">
+			     
+				 <ul><!--vai determinar que o HOME seja minha pagina principal-->
+				     <li><a href="<?php bloginfo('home'); ?>">Home</a></li>
+                     <?php wp_list_pages('title_li=');?>
+				 </ul><!--vai fazer com que as paginas criada pelo usuario apareça-->
+			 
+			 </div><!--fim do header pagina-->
+			 
+			 <div id="header-social">
+			     
+				 <a href="http://www.facebook.com/"><img src="<?php bloginfo('template_url'); ?>/imagens/icon-face.jpg" rel="" title="" /></a>
+				 <a href=""><img src="<?php bloginfo('template_url'); ?>/imagens/icon-google.jpg" rel="" title="" /></a>
+				 <a href=""><img src="<?php bloginfo('template_url'); ?>/imagens/icon-twist.jpg" rel="" title="" /></a>
+				 <a href=""><img src="<?php bloginfo('template_url'); ?>/imagens/icon-youtube.jpg" rel="" title="" /></a>
+			 
+			 </div><!--fim do header social-->
+		 
+		 </div><!--fim do header sup contente-->
+	 
+	 </div><!--fim do header superior-->
+
+<div id="header-content">
+     
+	 <div id="logo">
+	    <!--vai fazer com que o meu logo seja um botão para a pagina home-->
+		<a href="<?php bloginfo('home'); ?>"><img src="<?php bloginfo('template_url'); ?>/imagens/logo.png" rel="" title=""/></a>
+	 
+	 </div><!--fim logo-->
+	 
+	  <div id="search">
+	     
+		 <form action="" method="post">
+		     <input type="text" name="" placeholder="Digite sua pesquisa">
+			  <input type="submit" value="" class="btn-search" name="">
+		 </form>
+	 
+	 </div><!--fim search-->
+	 
+	 <div id="login">
+	    <!--mudar para um comando como o bloginfo() que ainda não sei-->
+	    <ul>
+		    <li class="logar"><a href="http://127.0.0.1/projects/php/My-First-TemplateWp/wordpress/wp-login.php?">Login</a></li>
+			<li class="registre"><a href="http://127.0.0.1/projects/php/My-First-TemplateWp/wordpress/wp-login.php?action=registre">Registre-se</a></li>
+		</ul>
+	 
+	 </div><!--fim login-->
+	 
+
+</div><!--fim do header contente-->
+
+<!--Categorias-->
+<div id="nav">
+     
+	 <div id="nav-content">
+	     
+		 <ul> <!--O title_li=&hide_empty= é o camando que faz aparecer as categoria caso eu de o valor ZERO no final, caso seja um valor maior que ZERO ele ira exibir somente categorias com postagems com determinado valor-->
+             <?php wp_list_categories('title_li=&hide_empty=0&orderby&depth=2&exclude=15'); ?> 
+		 </ul><!--O &orderby no final depois do valor de postagens vai fazer com que as categorias apareçam na ordem que o usuario ordenou
+	     <!--O &exclude= faz com que exclua determinada categoria, neste caso esta sendo usado para não danificar o a barra de menu-->
+	 </div><!--fim do nav-content-->
+
+</div><!--fim nav--> 
+
+</div><!--fim do hearder-->
---
+++ wordpress/wp-content/themes/My First Template WP/sidebar.php
@@ -0,0 +1,140 @@
+<div id="sidebar">
+     
+	 <div id="sidebar-vistos">
+	     <div id="title-vistos"><span>POST MAIS VISTOS</span></div>
+		 <ul>
+		     <li>
+			     <span class="vistos-number">1</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="vistos-number">2</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="vistos-number">3</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="vistos-number">4</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="vistos-number">5</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+		 </ul>
+	 
+	 </div><!--fim da sidebar vistos-->
+	 
+	 <div id="sidebar-colunistas">
+	     <div id="title-colunistas"><span>COLUNISTAS</span></div>
+		 <div class="colunistas">
+		     <ul>
+			     <li>
+				     <img src="<?php bloginfo('template_url'); ?>/imagens/colunistas.jpg" alt="" title=""/>
+					 <h1><a href="#">Roberto Pinto</a></h1>
+					 
+					 <div class="info-colunistas">
+						 <ul>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/facebook.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/google+.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/twitter.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/youtube.png" alt="" title=""/></a></li>
+						 </ul>
+					 </div><!--fim do info colunistas-->
+				 </li>
+				 <li>
+				     <img src="<?php bloginfo('template_url'); ?>/imagens/colunistas.jpg" alt="" title=""/>
+					 <h1><a href="#">Roberto Pinto</a></h1>
+					 
+					 <div class="info-colunistas">
+						 <ul>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/facebook.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/google+.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/twitter.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/youtube.png" alt="" title=""/></a></li>
+						 </ul>
+					 </div><!--fim do info colunistas-->
+				 </li>
+				 <li>
+				     <img src="<?php bloginfo('template_url'); ?>/imagens/colunistas.jpg" alt="" title=""/>
+					 <h1><a href="#">Roberto Pinto</a></h1>
+					 
+					 <div class="info-colunistas">
+						 <ul>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/facebook.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/google+.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/twitter.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/youtube.png" alt="" title=""/></a></li>
+						 </ul>
+					 </div><!--fim do info colunistas-->
+				 </li>
+				 <li>
+				     <img src="<?php bloginfo('template_url'); ?>/imagens/colunistas.jpg" alt="" title=""/>
+					 <h1><a href="#">Roberto Pinto</a></h1>
+					 
+					 <div class="info-colunistas">
+						 <ul>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/facebook.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/google+.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/twitter.png" alt="" title=""/></a></li>
+							 <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/imagens/youtube.png" alt="" title=""/></a></li>
+						 </ul>
+					 </div><!--fim do info colunistas-->
+				 </li>
+			 </ul>
+		 </div><!--fim do colunistas-->
+	 
+	 </div><!--fim da sidebar colunistas-->
+	 
+	 <div id="sidebar-publi">
+	     <div id="title-publi"><span>PUBLICIDADE</span></div>
+		     <ul>
+			     <li class="publi-maior">Publicidade maior</li>
+				 <li class="publi-left" >Publicidade left</li>
+				 <li class="publi-right">Publicidade right</li>
+			 </ul>
+	
+	 </div><!--fim da sidebar publi-->
+	 
+	 <div id="sidebar-coment">
+	   
+	     <div id="title-coment"><span>MAIS COMENTADOS</span></div>
+		 <ul>
+		     <li>
+			     <span class="coment-number">1</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="coment-number">2</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="coment-number">3</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="coment-number">4</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+			 <li>
+			     <span class="coment-number">5</span>
+				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+			 </li>
+		 </ul>
+	 
+	 </div><!--fim da sidebar coment-->
+	 
+	 <div id="sidebar-facebook">
+	     
+		 <div id="title-facebook"><span>FACEBOOK</span></div>
+		 <div id="face-box">
+		 <!--Segunda parte do codigo Fabook-->
+		 <div class="fb-like-box" data-href="https://www.facebook.com/FacebookDevelopers" data-width="280" data-height="300" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+	     <!--fim codigo Facebook-->
+		 </div>
+	 
+	 </div><!--fim da sidebar facebook-->
+
+</div><!--fim sidebar-->
