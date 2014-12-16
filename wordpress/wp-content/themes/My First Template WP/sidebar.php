e8dd7c9d69e8d2bde2be02fa1e8bd7188efb93c8
Roberto Pinto <robertowashington.bailao@gmail.com>
2014-12-16 16:41:39 +0000

    Configuration  in sidebar for most comments posts

--- wordpress/wp-content/themes/My First Template WP/sidebar.php
+++ wordpress/wp-content/themes/My First Template WP/sidebar.php
@@ -27,7 +27,7 @@
 				     <!--vai pegar o avatar do author o 70 representa 70 px de tamanho do avatar-->
 				     <?php echo (get_avatar($author, 70)); ?>
 				      <!--Este codigo faz com que o nome seja a url do autor e faz aparecer o primerio e ultimo nome do autor-->
-				     <h1><a href="<php bloginfo('url'); ?>/?author=<?php echo $author; ?>"><?php $user_info = get_userdata($author); echo ($user_info->first_name ."". $user_info->last_name . "\n"); ?></a></h1>
+				     <h1><a href="<?php bloginfo('url'); ?>/?author=<?php echo $author; ?>"><?php $user_info = get_userdata($author); echo ($user_info->first_name ."". $user_info->last_name . "\n"); ?></a></h1>
 					 
 					 <div class="info-colunistas">
 						 <ul> <!--ATENÇÂO NAO PRATICO. adicionado o url facebook e target name para abrir em outra pagina-->
@@ -59,26 +59,24 @@
 	   
 	     <div id="title-coment"><span>MAIS COMENTADOS</span></div>
 		 <ul>
-		     <li>
-			     <span class="coment-number">1</span>
-				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
+		     <!-- a variavel $wpdb que é pre-definida pelo WP vai ser responsavel por pegar os 
+			 resultados do banco de dados, e dentro do comando get_results(); nos informamos quais informações é preciso-->
+		     <?php $result = $wpdb->get_results("SELECT comment_count,ID,post_title  FROM $wpdb->posts ORDER BY comment_count DESC LIMIT 0 , 5"); //leia o codigo como ta escrito
+			 foreach($result as $post) {
+			     setup_postdata($post);
+				 $postid = $post->ID;
+				 $title = $post->post_title;
+				 $commentcount = $post->comment_count;
+				 if ($commentcount != 0) { ?> 
+				     
+			<li> 
+			     <!-- para o numero dos comentarios aparecer em ordem numerica usei a condição 
+                  $i = $i; $i++; echo $i; -->				 
+			     <span class="coment-number"><?php $i = $i; $i++; echo $i; ?></span>
+				 <a href="<?php the_Permalink(); ?>"><?php echo $title; ?></a>
 			 </li>
-			 <li>
-			     <span class="coment-number">2</span>
-				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
-			 </li>
-			 <li>
-			     <span class="coment-number">3</span>
-				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
-			 </li>
-			 <li>
-			     <span class="coment-number">4</span>
-				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
-			 </li>
-			 <li>
-			     <span class="coment-number">5</span>
-				 <a href="#">Google deve liberar nova interface do Maps em evento</a>
-			 </li>
+                 <?php } ?>
+				 <?php } ?>
 		 </ul>
 	 
 	 </div><!--fim da sidebar coment-->
